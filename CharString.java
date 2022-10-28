f09da1d4e4015ab0db1bda751822b60961d79880
JIHOONKONG <108924401+JIHOONKONG@users.noreply.github.com>
2022-10-29 05:09:08 +0900

    Add files via upload

---
+++ CharString.java
@@ -0,0 +1,20 @@
+
+public class CharString {
+
+	public static void main(String[] args) {
+		System.out.println('공');
+		
+		System.out.println("공지훈");
+		
+		//System.out.println('셍활코딩');
+		
+		System.out.println("공");
+		
+		System.out.println("공지훈" + " 입니다.");
+		
+		System.out.println("1+1" + "은 2입니다.");
+		
+		System.out.println("Jihoon said\n \"Welcome programming World!\"");
+	}
+
+}
