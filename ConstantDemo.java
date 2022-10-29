b3a273940d171431c19d75e0af20bef24446916e
JIHOONKONG <108924401+JIHOONKONG@users.noreply.github.com>
2022-10-30 06:51:02 +0900

    Add files via upload

---
+++ ConstantDemo.java
@@ -0,0 +1,14 @@
+
+public class ConstantDemo {
+
+	public static void main(String[] args) {
+		//int a = 2.2; 2.2는 실수라 오류가 난다.
+		float a = 2.2F;
+		long b = 2147483648L;
+		byte c = 100;// byte,short는 Integer허용
+		
+		double d = 3.0F;
+		System.out.println(d);
+	}
+
+}
---
+++ NotDemo.java
@@ -0,0 +1,16 @@
+
+public class NotDemo {
+
+	public static void main(String[] args) {
+		System.out.println(1!=2);
+		System.out.println(1!=1);
+		System.out.println("one" != "two");
+		System.out.println("one" != "one");
+		
+		System.out.println(10>20);
+		System.out.println(10>2);
+		System.out.println(10>10);
+
+	}
+
+}
